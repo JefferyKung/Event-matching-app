47ab09f0e1a92ea2c2b7c03717d127435aa6d945
JefferyKung <kung06142003@gmail.com>
2023-02-21 03:57:05 +0800

    remove two await to try to fix bugs

--- src/utils/mongo.client.js
+++ src/utils/mongo.client.js
@@ -3,7 +3,7 @@ import * as Realm from 'realm-web';
 export const app = new Realm.App({ id: import.meta.env.VITE_REALM_APP_ID })
 
 const credentials = Realm.Credentials.anonymous()
-export const user = await app.logIn(credentials);
+export const user = app.logIn(credentials);
 
 //connect to the database
 const mongo = app.currentUser.mongoClient("mongodb-atlas")
